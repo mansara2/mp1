02d3e0621c2d14679d319960301ff8827baa3d56
Alhaji <mansara2@illinois.edu>
2015-02-22 02:41:59 -0600

    scroll top

---
+++ js/posindicator.js
@@ -0,0 +1,15 @@
+$(document).ready(
+	function(){
+		$(window).scroll(
+			function(){
+				//if($().offset.top == ){
+				//}
+				//if(){
+				//}
+				//if(){
+				//}
+				alert($(window).scrollTop(););
+			}
+		);
+	}
+);
